715e16624c89df9c8f08c9ce437dd05f0546c202
naderae <naderabouezze93@gmail.com>
2017-08-21 16:13:50 -0400

    added third button, no functionality yet

--- ajax.js
+++ ajax.js
@@ -1,5 +1,49 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  /* Your code goes here */
 
+  var button1 = document.getElementById('button1');
+  var button2 = document.getElementById('button2');
+  var button3 = document.getElementById('button3');
+
+  button1.addEventListener('click', function(){
+    $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com',
+    method: 'GET',
+    data: '{}',
+    dataType: 'text'
+    });
+  })
+
+
+  button2.addEventListener('click', function(){
+    $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/ping',
+    method: 'GET',
+    dataType: 'text'
+    }).done(function (responseData) {
+      console.log('Success!');
+      console.log(responseData);
+      var responseString = document.createElement('p');
+      responseString.innerHTML = responseData;
+      document.querySelector('#step3456').append(responseString);
+    }).fail(function () {
+      console.log('fail!');
+      var niceMessage = document.createElement('p');
+      niceMessage.innerHTML = "Whoops! Something wrong has occured. Please Try Again!";
+      document.querySelector('#step3456').append(niceMessage);
+    }).always(function () {
+      console.log("Hey the request finished!");
+    });
+  });
+
+  button3.addEventListener('click', function(){
+    $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/count',
+    method: 'GET',
+    dataType: 'text'
+  });
+
+
+
+});
 });
